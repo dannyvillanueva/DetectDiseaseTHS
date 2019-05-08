4e8029a489cbd556855ecdc3d05289f9c2642870
Danny G Villanueva Vega <danny.villanueva1@upr.edu>
2019-05-08 18:50:42 -0400

    pull changes

--- ths/test/testcontractions.py
+++
@@ -1,9 +0,0 @@
-from ths.utils.contractions import expandContractions
-
-def main():
-    text = "Donald's trump isn't crazy, he's just a mother fucker."
-    text2 = expandContractions(text)
-    print("text: ", text)
-    print("text2: ", text2)
-if __name__ == "__main__":
-    main()
--- ths/test/testlogistic.py
+++
@@ -1,8 +0,0 @@
-from ths.nn.sequences.processlogistic import ProcessTweetsLogistic, ProcessTweetsConv1D
-def main():
-    print("Working:")
-    P = ProcessTweetsConv1D("data/cleantextlabels5.csv", "trained/embedding3.csv")
-    P.process(epochs=15)
-
-if __name__ == "__main__":
-        main()
