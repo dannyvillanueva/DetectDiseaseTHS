9ee3817abaaddfc99ac2366ae3c9e8836373d8da
Danny G Villanueva Vega <danny.villanueva1@upr.edu>
2019-05-20 03:56:43 -0400

    build inputs triplets

---
+++ ths/test/main_build_inputs.py
@@ -0,0 +1,264 @@
+import json
+from ths.utils.contractions import expandContractions
+import re
+import html
+from autocorrect import spell
+import random
+import csv
+import numpy as np
+
+
+def get_disease(t):
+    list_disease = ['zika','flu','ebola','measles','diarrhea']
+    for d in list_disease:
+        if d in t.split():
+            return d
+    return False
+
+
+def has_disease(t):
+    list_disease = ['zika','flu','ebola','measles','diarrhea']
+    for d in list_disease:
+        if d in t.split():
+            return True
+    return False
+
+
+def get_single_file(i):
+    try:
+        cc = 0
+        datafile = open("data/labeling_data_student/labeling_data_student_"+str(i)+".csv", "r")
+        relevance_input = []
+        with datafile as f:
+            for row in csv.reader(f):
+                if cc > 0:
+                    newline1 = " ".join(row[1].split())
+                    newline2 = " ".join(row[2].split())
+                    newline3 = " ".join(row[3].split())
+                    if newline1.strip() == 'premise':
+                        print("file ", i, "cc ", cc)
+                    if row[4] != '' and row[5] != '' and newline3.strip() != "" and newline2.strip() != "" \
+                            and newline1.strip() != "":
+                        relevance_input.append([newline1, newline2, newline3, float(row[4]), float(row[5])])
+                cc += 1
+    except FileNotFoundError as e:
+        print("No file ", i)
+        return 0
+    return relevance_input
+
+
+def get_data():
+    data = []
+    for i in range(1,54):
+        result = get_single_file(i)
+        if result is not 0:
+            data = data + result
+    return np.array(data)
+
+
+def average_relevance(data):
+    new_set = set()
+    new_data = []
+    cc = 0
+    for row in data:
+        t0 = row[0]
+        t1 = row[1]
+        t2 = row[2]
+        text_1 = t0+t1+t2
+        temp_data = [row]
+        if text_1 not in new_set:
+            for i in range(cc+1, len(data)):
+                row_2 = data[i]
+                text_2 = row_2[0]+row_2[1]+row_2[2]
+                if text_1 == text_2:
+                    temp_data.append(row_2)
+            #average
+            temp_data = np.array(temp_data)
+            r1 = temp_data[:, 3].astype(np.float).mean()
+            r2 = temp_data[:, 4].astype(np.float).mean()
+            new_triplet = [t0, t1, t2, r1, r2]
+            new_set.add(text_1)
+            new_data.append(new_triplet)
+        cc += 1
+    return new_data
+
+
+def set_disease(d):
+    data = []
+    diseases = {'flu': 0, 'measles': 1, 'diarrhea': 2, 'ebola': 3, 'zika': 4}
+    for x in d:
+        dis_1 = get_disease(x[0])
+        dis_2 = get_disease(x[1])
+        dis_3 = get_disease(x[2])
+        data.append([x[0], diseases[dis_1], x[1], diseases[dis_2], x[2], diseases[dis_3], x[3], x[4]])
+    return data
+
+
+def set_labels(d):
+    #labeled_data = load_np_data('data/cleantextlabels7.npy')
+    #new_data = dict(zip(labeled_data[:, 0], labeled_data[:, 1]))
+    data = []
+
+    for x in d:
+        #key = x[0]
+        #print(key)
+        #label1 = new_data[key]
+        #print(label1)
+        #label2 = labeled_data_dict[x[2]]
+        #label3 = labeled_data_dict[x[4]]
+        data.append([x[0], x[1], 1, x[2], x[3], 1, x[4], x[5], 1, x[6], x[7]])
+    return data
+
+
+def set_global_relevance(d):
+    data = []
+    for x in d:
+        x = list(x)
+        if x[9]>= x[10]:
+            r1 = 1
+        else:
+            r1= 0
+        x.append(r1)
+        data.append(x)
+    return data
+
+
+def load_np_data(f):
+    d = np.load(f)
+    d = d[d[:, 0].argsort()]
+    return d
+
+
+def get_sets_triplets(d):
+    flu = []
+    ebola = []
+    measles = []
+    diarrhea = []
+
+    for x in d:
+        disease = get_disease(x[0])
+        if disease == 'flu':
+            flu.append(x)
+        if disease == 'ebola':
+            ebola.append(x)
+        if disease == 'measles':
+            measles.append(x)
+        if disease == 'diarrhea':
+            diarrhea.append(x)
+    return flu, ebola, measles, diarrhea
+
+
+def get_sets_datalabels(dis):
+    labeled_data = load_np_data('data/cleantextlabels7.npy')
+    list_d = []
+    for row in labeled_data:
+        disease = get_disease(row[0])
+        if disease == dis:
+            list_d.append(row)
+    return list_d
+
+
+def set_input_same_class(d):
+    flu, ebola, measles, diarrhea = get_sets_triplets(d)
+    data = []
+    triplet_set = set()
+    list_disease = ['flu', 'ebola', 'measles', 'diarrhea']
+    for dis in list_disease:
+        print(dis)
+        for i in range(2000):
+            disease = False
+            while not disease:
+                if dis == 'flu':
+                    triplet_all = random.choice(flu)
+                if dis == 'ebola':
+                    triplet_all = random.choice(ebola)
+                if dis == 'measles':
+                    triplet_all = random.choice(measles)
+                if dis == 'diarrhea':
+                    triplet_all = random.choice(diarrhea)
+                triplet_first = triplet_all[0]
+                dis2 = get_disease(triplet_first)
+                if dis2 == dis:
+                    disease = True
+            cont = 0
+            while cont == 0:
+                h3 = random.choice(get_sets_datalabels(dis))
+                if get_disease(h3[0]) == dis and int(h3[1]) == 0:
+                    cont = 1
+            if dis == dis2 and triplet_first not in triplet_set:
+                list_dis = {'flu': 0, 'measles': 1, 'diarrhea': 2, 'ebola': 3, 'zika': 4}
+                data.append([triplet_all[0],triplet_all[1], triplet_all[2], triplet_all[3], triplet_all[4],
+                              triplet_all[5], h3[0], list_dis[dis], int(h3[1]), triplet_all[9], 0.05])
+            triplet_set.add(triplet_first)
+            print("disease", dis, "len data: ", len(data), " steps ", i)
+            if len(data) >= 800:
+                i = 2000
+            else:
+                i += 1
+    data = set_global_relevance(data)
+    new_data = d + data
+    return new_data
+
+
+def set_input_outof_class(d):
+    flu, ebola, measles, diarrhea = get_sets_triplets(d)
+    data = []
+    triplet_set = set()
+    list_disease = ['flu', 'ebola', 'measles', 'diarrhea']
+    for dis in list_disease:
+        print(dis)
+        for i in range(2000):
+            disease = False
+            while not disease:
+                if dis == 'flu':
+                    triplet_all = random.choice(flu)
+                if dis == 'ebola':
+                    triplet_all = random.choice(ebola)
+                if dis == 'measles':
+                    triplet_all = random.choice(measles)
+                if dis == 'diarrhea':
+                    triplet_all = random.choice(diarrhea)
+                triplet_first = triplet_all[0]
+                dis2 = get_disease(triplet_first)
+                if dis2 == dis:
+                    disease = True
+            cont = 0
+            list_dis = {'flu': 0, 'measles': 1, 'diarrhea': 2, 'ebola': 3, 'zika': 4}
+            while cont == 0:
+                list_dis_2 = ['flu', 'measles', 'ebola', 'diarrhea']
+                random.shuffle(list_dis_2)
+                for row_lis in list_dis_2:
+                    if row_lis != dis:
+                        h3_dis = row_lis
+                h3 = random.choice(get_sets_datalabels(h3_dis))
+                if get_disease(h3[0]) != dis and int(h3[1]) == 0:
+                    cont = 1
+            if dis == dis2 and triplet_first not in triplet_set:
+                data.append([triplet_all[0],triplet_all[1], triplet_all[2], triplet_all[3], triplet_all[4],
+                              triplet_all[5], h3[0], list_dis[h3_dis], int(h3[1]), triplet_all[9], 0.0])
+            triplet_set.add(triplet_first)
+            print("disease", dis, "len data: ", len(data), " steps ", i)
+            if len(data) >= 800:
+                i = 2000
+            else:
+                i += 1
+    data = set_global_relevance(data)
+    new_data = d + data
+    return new_data
+
+
+if __name__ == "__main__":
+    final_data = get_data()
+    print("finish data loading", len(final_data))
+    final_data = average_relevance(final_data)
+    print("finish average relevance", len(final_data))
+    final_data = set_disease(final_data)
+    print("finish set disease", len(final_data))
+    final_data = set_labels(final_data)
+    print("finish set labels", len(final_data))
+    final_data = set_global_relevance(final_data)
+    print("finish set global relevance", len(final_data))
+    final_data = set_input_same_class(final_data)
+    print("finish add triplets same class", len(final_data))
+    final_data = set_input_outof_class(final_data)
+    print("finish add triplets out of class", len(final_data))
---
+++ ths/test/testtweetdata.py
@@ -0,0 +1,446 @@
+import json
+from ths.utils.contractions import expandContractions
+import re
+import html
+from autocorrect import spell
+import random
+import csv
+import numpy as np
+import sys
+#import os
+#os.chdir(os.getcwd())
+
+def reduce_lengthening(text):
+    pattern = re.compile(r"(.)\1{2,}")
+    return pattern.sub(r"\1\1", text)
+
+def fix_text_format(row):
+    text = row.lower()  # lowercase text
+    text = text.replace("\n", ' ')  # remove line break
+    text = re.sub(r'http\S+', '', text)  # remove links
+    text = re.sub(r'#\S+', '', text)  # remove hashtag
+    text = re.sub(r'@\S+', '', text)  # remove mentions
+    text = " ".join(text.split())  # remove duplicate spaces
+    text = html.unescape(text)
+    text = text.replace("“", ' ').replace("”", ' ').replace('"', ' ')  # remove quotation marks
+    text = text.replace("’", "'")
+    text = " ".join(reduce_lengthening(word) for word in text.split())
+    text = expandContractions(text)  # expand contractions
+    text = " ".join("i am" if word == "im" else word for word in text.split())
+    text = re.sub(r"[^A-Za-z0-9 ]+", '', text)
+    text = " ".join(spell(word) for word in text.split())
+    text = " ".join(text.split())  # remove duplicate spaces
+    text = text.lower()
+    return text
+
+
+def get_random_tweet(tl, item, disease_item):
+    rand_item = random.choice(tl)
+    max_rec = 1000
+    counter = 0
+    if disease_item in rand_item[0].split() and item != str(rand_item[0]):
+        return rand_item
+    else:
+        counter +=1
+        if counter < max_rec:
+            get_random_tweet(tl, item, disease_item)
+        else:
+            return False
+
+
+def get_disease(t):
+    list_disease = ['zika','flu','ebola','measles','diarrhea']
+    for d in list_disease:
+        if d in t.split():
+            return d
+    return False
+
+
+def has_disease(t):
+    list_disease = ['zika','flu','ebola','measles','diarrhea']
+    for d in list_disease:
+        if d in t.split():
+            return True
+    return False
+
+
+def main1():
+    with open("data/raw_tweet100k.json") as json_data:
+        file = json_data.readlines()
+        selected_data = file[20000:80000]
+        cc = 0
+        #selected_data = file[20000:21000]
+        hash_tweet = {}
+        for row in selected_data:
+            full_tweet = json.loads(row)
+            tweet_text = full_tweet["extended_tweet"]["full_text"]
+            tweet_id = full_tweet['id_str']
+            text_formatted = fix_text_format(tweet_text)
+            if has_disease(text_formatted):
+                hash_tweet[text_formatted] = tweet_id
+                cc += 1
+                print("cc: ", cc)
+    tweet_list = []
+    for k, v in hash_tweet.items():
+        tweet_list.append([k, v])
+    #tweet_list_final = tweet_list[:14999]
+    tweet_list_final = tweet_list
+    print(len(tweet_list_final), " sentences selected from ", len(hash_tweet))
+    pair_sentences = []
+    goal_tweets = 15000
+    count = 0
+    for i in tweet_list_final:
+        if count < goal_tweets:
+            disease = get_disease(str(i[0]))
+            rand_value = get_random_tweet(tweet_list, str(i[0]), disease)
+            if rand_value:
+                print("pair(disease): ", disease, get_disease(rand_value[0]))
+                pair_sentences.append([str(i[1]),str(i[0]), str(rand_value[1]), str(rand_value[0])])
+                count +=1
+            else:
+                print("function do not found a pair tweet with the same disease")
+    print("pair sentences array built: ", len(pair_sentences))
+    slice = []
+    labelers = 53
+    counter = 1
+    repeat_number = 3
+    for row in pair_sentences:
+        for _ in range(repeat_number):
+            fields = [str(counter), str(row[0]), str(row[1]), str(row[2]), str(row[3])]
+            counter += 1
+            if counter > labelers:
+                counter = 1
+            slice.append(fields)
+    print("sentences distribution built")
+    #np.save('data/data_to_labeling', np.array(slice))
+    np.save('data/data_to_labeling_test', np.array(slice))
+    #outfile = open('data/data_to_labeling.csv', 'w', newline='')
+    outfile = open('data/data_to_labeling_test.csv', 'w', newline='')
+    writer = csv.writer(outfile)
+    writer.writerow(["Classifier", "id1", "premise", "id2", "hypothesis"])
+    writer.writerows(slice)
+    outfile.close()
+    print("process completed")
+
+
+def main2():
+    try:
+        cc = 0
+        datafile = open("data/cleantextlabels7.csv", "r")
+        with datafile as f:
+            hash_tweet = {}
+            for row in csv.reader(f) :
+                newline = " ".join(row[0].split())
+                tweet_text = newline.replace("’", "'")
+                text_formatted = fix_text_format(tweet_text)
+                label =  int(row[1])
+                if has_disease(text_formatted) and label == 1:
+                    hash_tweet[text_formatted] = text_formatted
+                    cc +=1
+                    print("cc: ", cc)
+    except Exception as e:
+        print(e)
+
+    tweet_list = []
+    for k, v in hash_tweet.items():
+        tweet_list.append([k, v])
+    #tweet_list_final = tweet_list[:14999]
+    tweet_list_final = tweet_list
+    print(len(tweet_list_final), " sentences selected from ", len(hash_tweet))
+    pair_sentences = []
+    goal_tweets = 15000
+    count = 0
+    for i in tweet_list_final:
+        if count < goal_tweets:
+            disease = get_disease(str(i[0]))
+            rand_value = get_random_tweet(tweet_list, str(i[0]), disease)
+            if rand_value:
+                print("pair(disease): ", disease, get_disease(rand_value[0]))
+                pair_sentences.append([str(i[0]), str(rand_value[0])])
+                count +=1
+            else:
+                print("function do not found a pair tweet with the same disease")
+    print("pair sentences array built: ", len(pair_sentences))
+    slice = []
+    labelers = 53
+    counter = 1
+    repeat_number = 3
+    for row in pair_sentences:
+        for _ in range(repeat_number):
+            fields = [str(counter), str(row[0]), str(row[1])]
+            counter += 1
+            if counter > labelers:
+                counter = 1
+            slice.append(fields)
+    print("sentences distribution built")
+    #np.save('data/data_to_labeling', np.array(slice))
+    np.save('data/data_to_labeling_test', np.array(slice))
+    #outfile = open('data/data_to_labeling.csv', 'w', newline='')
+    outfile = open('data/data_to_labeling_test.csv', 'w', newline='')
+    writer = csv.writer(outfile)
+    writer.writerow(["Classifier", "premise", "hypothesis"])
+    writer.writerows(slice)
+    outfile.close()
+    print("process completed")
+
+
+def main3():
+    try:
+        cc = 0
+        c= 0
+        datafile = open("data/cleantextlabels7.csv", "r")
+        with datafile as f:
+            flu = set()
+            zika = set()
+            ebola = set()
+            measles = set()
+            diarrhea = set()
+            for row in csv.reader(f) :
+                #if cc < 100:
+                newline = " ".join(row[0].split())
+                tweet_text = newline.replace("’", "'")
+                text_formatted = fix_text_format(tweet_text)
+                label = int(row[1])
+                if label == 1 and has_disease(text_formatted):
+                    disease = get_disease(text_formatted)
+                    if disease == 'flu':
+                        flu.add(text_formatted)
+                    if disease == 'zika':
+                        zika.add(text_formatted)
+                    if disease == 'ebola':
+                        ebola.add(text_formatted)
+                    if disease == 'measles':
+                        measles.add(text_formatted)
+                    if disease == 'diarrhea':
+                        diarrhea.add(text_formatted)
+                    cc += 1
+                    print(cc, "from", c, "disease:", disease,"label:", label)
+                c += 1
+    except Exception as e:
+        print(e)
+    print("flu_len: ", len(flu))
+    print("zika_len: ", len(zika))
+    print("ebola_len: ", len(ebola))
+    print("measles_len: ", len(measles))
+    print("diarrhea_len: ", len(diarrhea))
+
+    list_disease = ['zika', 'flu', 'ebola', 'measles', 'diarrhea']
+    triplet = []
+    for d in list_disease:
+        if d == 'zika':
+            list_set = zika
+        if d == 'flu':
+            list_set = flu
+        if d == 'ebola':
+            list_set = ebola
+        if d == 'measles':
+            list_set = measles
+        if d == 'diarrhea':
+            list_set = diarrhea
+        temp_triplet = []
+        if len(list_set) > 2:
+            count = 1
+            list_set = list(list_set)
+            random.shuffle(list_set)
+            if len(list_set) >= 1000:
+                num_samples = 1000
+            else:
+                num_samples = len(list_set)
+            for i in range(num_samples - 2):
+                for j in range(num_samples - 2 - i):
+                    for k in range(num_samples - j - 2 - i):
+                        temp_triplet.append([list_set[i], list_set[j + i + 1], list_set[k + 2 + j + i]])
+                print(d,"- step:", count, "/", num_samples)
+                count += 1
+            random.shuffle(temp_triplet)
+
+            temp_triplet = temp_triplet[:1000]
+            triplet = triplet+temp_triplet
+        print("total triplets: ", d, len(temp_triplet))
+    print("triplets sentences built: ", len(triplet))
+    slice = []
+    labelers = 53
+    counter = 1
+    repeat_number = 3
+    for row in triplet:
+        for _ in range(repeat_number):
+            fields = [str(counter), str(row[0]), str(row[1]), str(row[2])]
+            counter += 1
+            if counter > labelers:
+                counter = 1
+            slice.append(fields)
+    print("sentences distribution built")
+    np.save('data/data_to_labeling_original3', np.array(triplet))
+    np.save('data/data_to_labeling_test3', np.array(slice))
+    #outfile = open('data/data_to_labeling.csv', 'w', newline='')
+    outfile = open('data/data_to_labeling_test3.csv', 'w', newline='')
+    writer = csv.writer(outfile)
+    writer.writerow(["Classifier", "premise", "h1", "h2"])
+    writer.writerows(slice)
+    outfile.close()
+    print("process completed")
+
+
+def main4():
+    try:
+        data = load_data('data/data_to_labeling_test3.npy')
+        set1 = data[:, 1]
+        set2 = data[:, 2]
+        set3 = data[:, 3]
+        allsets = np.concatenate((set1, set2, set3), axis=0)
+
+        cc = 0
+        c = 0
+        datafile = open("data/cleantextlabels7.csv", "r")
+        with datafile as f:
+            flu = set()
+            zika = set()
+            ebola = set()
+            measles = set()
+            diarrhea = set()
+            for row in csv.reader(f):
+                # if cc < 100:
+                newline = " ".join(row[0].split())
+                tweet_text = newline.replace("’", "'")
+                text_formatted = fix_text_format(tweet_text)
+                label = int(row[1])
+                if label == 1 and has_disease(text_formatted) and text_formatted not in allsets:
+                    disease = get_disease(text_formatted)
+                    if disease == 'flu':
+                        flu.add(text_formatted)
+                    if disease == 'zika':
+                        zika.add(text_formatted)
+                    if disease == 'ebola':
+                        ebola.add(text_formatted)
+                    if disease == 'measles':
+                        measles.add(text_formatted)
+                    if disease == 'diarrhea':
+                        diarrhea.add(text_formatted)
+                    cc += 1
+                    print(cc, "from", c, "disease:", disease, "label:", label)
+
+                if cc % 10 == 0:
+                    print("flu_len: ", len(flu))
+                    print("zika_len: ", len(zika))
+                    print("ebola_len: ", len(ebola))
+                    print("measles_len: ", len(measles))
+                    print("diarrhea_len: ", len(diarrhea))
+
+                if len(flu) > 80 and len(measles) > 80 and len(diarrhea) > 80:
+                    print("sets full")
+                    break
+
+                c += 1
+    except Exception as e:
+        print(e)
+    print("flu_len: ", len(flu))
+    print("zika_len: ", len(zika))
+    print("ebola_len: ", len(ebola))
+    print("measles_len: ", len(measles))
+    print("diarrhea_len: ", len(diarrhea))
+
+    list_disease = ['zika', 'flu', 'ebola', 'measles', 'diarrhea']
+    triplet = []
+    for d in list_disease:
+        if d == 'zika':
+            list_set = zika
+        if d == 'flu':
+            list_set = flu
+        if d == 'ebola':
+            list_set = ebola
+        if d == 'measles':
+            list_set = measles
+        if d == 'diarrhea':
+            list_set = diarrhea
+        temp_triplet = []
+        if len(list_set) > 2:
+            count = 1
+            list_set = list(list_set)
+            random.shuffle(list_set)
+            if len(list_set) >= 80:
+                num_samples = 80
+            else:
+                num_samples = len(list_set)
+            for i in range(num_samples - 2):
+                for j in range(num_samples - 2 - i):
+                    for k in range(num_samples - j - 2 - i):
+                        temp_triplet.append([list_set[i], list_set[j + i + 1], list_set[k + 2 + j + i]])
+                print(d, "- step:", count, "/", num_samples)
+                count += 1
+            random.shuffle(temp_triplet)
+
+            temp_triplet = temp_triplet[:75]
+            triplet = triplet + temp_triplet
+        print("total triplets: ", d, len(temp_triplet))
+    print("triplets sentences built: ", len(triplet))
+    slice = []
+    labelers = 3
+    counter = 1
+    repeat_number = 3
+    for row in triplet:
+        for _ in range(repeat_number):
+            fields = [str(counter), str(row[0]), str(row[1]), str(row[2])]
+            counter += 1
+            if counter > labelers:
+                counter = 1
+            slice.append(fields)
+    print("sentences distribution built")
+    np.save('data/data_to_labeling_original4', np.array(triplet))
+    np.save('data/data_to_labeling_test4', np.array(slice))
+    # outfile = open('data/data_to_labeling.csv', 'w', newline='')
+    outfile = open('data/data_to_labeling_test4.csv', 'w', newline='')
+    writer = csv.writer(outfile)
+    writer.writerow(["Classifier", "premise", "h1", "h2"])
+    writer.writerows(slice)
+    outfile.close()
+    print("process completed")
+
+
+def main5():
+    try:
+        datafile = open("data/cleantextlabels7.csv", "r")
+        new_file = []
+        with datafile as f:
+            for row in csv.reader(f):
+                newline = " ".join(row[0].split())
+                tweet_text = newline.replace("’", "'")
+                text_formatted = fix_text_format(tweet_text)
+                label = int(row[1])
+                new_file.append([text_formatted, label])
+        np.save("data/cleantextlabels7", new_file)
+    except FileNotFoundError as e:
+        print(e)
+
+
+def save_file(st, sl, l):
+    outfile = open(l+st+'.csv', 'w', newline='')
+    writer = csv.writer(outfile)
+    writer.writerow(["Classifier", "premise", "h1", "h2"])
+    writer.writerows(sl)
+    outfile.close()
+    print("file student", st, "saved")
+
+
+def save_multi_file_csv(l, d):
+    student = '1'
+    slice = []
+    for row in d:
+        if student != row[0]:
+            save_file(student, slice, l)
+            student = row[0]
+            slice = []
+        fields = [student, str(row[1]), str(row[2]), str(row[3])]
+        slice.append(fields)
+    save_file(student, slice, l)
+
+
+def load_data(f):
+    d = np.load(f)
+    d = d[d[:, 0].argsort()]
+    return d
+
+
+if __name__ == "__main__":
+    main5()
+    #data = load_data('data/data_to_labeling_test4.npy')
+    #save_multi_file_csv('data/new_labeling_data_student_', data)
